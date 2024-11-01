7bd560b8ae81b2a9c56eaa81e46195f25002e25a
Sanjiv-Thapa <sanjivthapa40@gmail.com>
2024-11-01 10:55:03 +0545

    initial commit

--- app/core/embeddings.py
+++
@@ -1,6 +0,0 @@
-from langchain_openai.embeddings import OpenAIEmbeddings
-from config.settings import settings
-
-
-def get_embeddings():
-    return OpenAIEmbeddings(openai_api_key=settings.openai_api_key)
